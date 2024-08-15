79324b1602cc0b1efaff8f63e54c18a0e616a701
Kartik527 <67816687+kartik5278@users.noreply.github.com>
2024-08-15 12:37:20 +0530

    Task/app setup (#2)
    * Firebase setup completed
    
    * Firebase setup android
    
    ---------
    
    Co-authored-by: kartikCygbit <kartik.parmar@cygbit.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/features/auth/screen/login_screen.dart';
+import 'package:reddit_clone/firebase_options.dart';
+import 'package:reddit_clone/theme/pallete.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,30 +20,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'Reddit tutorial',
+      theme: Pallete.darkModeAppTheme,
+      home: const LoginScreen(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Home Page'),
-      ),
-    );
-  }
-}
+// 36.32
